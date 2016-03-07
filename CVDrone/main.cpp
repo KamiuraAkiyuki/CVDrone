2dd86d39520c9e79a461629f408008a4d6cbd9a1
Kamiura Akiyuki <bxp5dkrmrz8ce@gmail.com>
2016-03-08 01:51:10 +0900

    Initial Commit

---
+++ CVDrone/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CVDrone
+//
+//  Created by Akiyuki Kamiura on 2016/03/08.
+//  Copyright © 2016年 Akiyuki Kamiura. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
